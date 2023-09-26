29d44efd57e1796d4aed1c49f438b9fa61a6678a
Ambraaaliu <cl6376@nyu.edu>
2023-09-27 01:52:18 +0200

    first update

---
+++ InvisibleStories/script.js
@@ -0,0 +1,59 @@
+// variable: assign value and stores data
+// let name = "Ambra"
+// let score = 2390
+// let isVisible = true;
+
+// // function:
+
+// function showLinks(){
+//     let theLinks = document.getElementById("links")
+//     theLinks.style.display = "block"
+// }
+
+// function hideLinks(){
+//     let theLinks = document.getElementById("links")
+//     theLinks.style.display = "none"
+// }
+
+// function showLinksOnImage() {
+//     let theLinks = document.getElementById("links");
+//     theLinks.style.display = "block";
+// }
+
+// function hideLinksOnImage() {
+//     let theLinks = document.getElementById("links");
+//     theLinks.style.display = "none";
+// }
+// function greet(){
+
+//     let theElement = document.getElementById("welcome-message")
+//     theElement.innerText = "hello, " + name
+
+
+//     console.log("hello, " + name)
+// }
+
+// window.addEventListener("scroll", function() {
+//     const header = document.getElementById("main-header");
+//     if (window.scrollY > 100) { // 当滚动超过一定距离时
+//         header.classList.add("header-scroll");
+//     } else {
+//         header.classList.remove("header-scroll");
+//     }
+// });
+
+// script.js
+// const leftImage = document.getElementById("left-image");
+// const rightImage = document.getElementById("right-image");
+// const pageContent = document.getElementById("page-content");
+
+
+// document.addEventListener("click", () => {
+//     leftImage.style.transform = "translateX(-100%)";
+//     rightImage.style.transform = "translateX(100%)";
+
+//     // 添加延迟以等待动画完成
+//     setTimeout(() => {
+//         pageContent.style.display = "block";
+//     }, 1000); // 1秒后显示内容
+// });
